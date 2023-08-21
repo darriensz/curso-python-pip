2096471378f0fea0963e7e584355b82e0f2b9f81
Darrien Sequera <darriensz@DESKTOP-PLPG44L>
2023-08-21 10:47:38 -0400

    fastapi

--- csv/main.py
+++ csv/main.py
@@ -1,15 +1,25 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
+
 
 def run():
-  data = read_csv.read_csv('data.csv')
+  #Código para generar el pie chart sin usar PANDAS
+  '''
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
-
   countries = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
-  charts.generate_pie_chart(countries, percentages)
+  '''
   
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] == 'South America']
+
+  countries = df[''Country/Territory''].values
+  percentages = df['World Population Percentage'].values
+
+  charts.generate_pie_chart(countries, percentages)
+  data = read_csv.read_csv('data.csv')#revisar migración de esta linea a pandas
   country = input('Type Country => ')
 
   result = utils.population_by_country(data, country)
